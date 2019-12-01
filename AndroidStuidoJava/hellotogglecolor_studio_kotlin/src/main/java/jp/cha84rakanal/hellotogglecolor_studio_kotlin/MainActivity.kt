56c4593d92ff4fc3c851a8e60cbed70b70d619cd
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-12-01 18:49:44 +0900

    プロジェクトを追加

---
+++ AndroidStuidoJava/hellotogglecolor_studio_kotlin/src/main/java/jp/cha84rakanal/hellotogglecolor_studio_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.cha84rakanal.hellotogglecolor_studio_kotlin
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
