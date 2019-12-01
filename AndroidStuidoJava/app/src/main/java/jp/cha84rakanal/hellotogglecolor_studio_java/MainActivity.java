77a3dd3a243f317d03f3a3be3388c6178377413c
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-12-01 15:49:09 +0900

    ボタンイベント

--- AndroidStuidoJava/app/src/main/java/jp/cha84rakanal/hellotogglecolor_studio_java/MainActivity.java
+++ AndroidStuidoJava/app/src/main/java/jp/cha84rakanal/hellotogglecolor_studio_java/MainActivity.java
@@ -2,6 +2,8 @@ package jp.cha84rakanal.hellotogglecolor_studio_java;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void onButtonClick(View v){
+        Log.v(MainActivity.class.getSimpleName(),"onButtonClick");
     }
 }
