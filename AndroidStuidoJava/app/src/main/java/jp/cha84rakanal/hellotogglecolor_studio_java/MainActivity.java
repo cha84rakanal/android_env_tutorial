af3feb9535c9e56591e53c6d7bbd7184781b077a
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-12-01 16:12:46 +0900

    ボタンを押すと色が変わる

--- AndroidStuidoJava/app/src/main/java/jp/cha84rakanal/hellotogglecolor_studio_java/MainActivity.java
+++ AndroidStuidoJava/app/src/main/java/jp/cha84rakanal/hellotogglecolor_studio_java/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
+    private boolean mToggle = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +17,7 @@ public class MainActivity extends Activity {
 
     void onButtonClick(View v){
         Log.v(MainActivity.class.getSimpleName(),"onButtonClick");
+        mToggle = !mToggle;
+        findViewById(R.id.back).setBackgroundColor( mToggle? 0xFFFF0000 : 0xFF0000FF);
     }
 }
